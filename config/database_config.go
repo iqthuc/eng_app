b01f3bfafff9b2f30ac790c49f44c3e0a5f6aa0e
iqthuc <quangthuc2506@gmail.com>
2023-10-30 00:04:59 +0700

    init project

---
+++ config/database_config.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConfigDatabase() (*sql.DB, error) {
+	username := "root"
+	password := "root"
+	host := "localhost:3306"
+	database := "english_app_db"
+
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, host, database)
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+
+	// "log"
+	database "eng_app_module/config"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func getSomeData(w http.ResponseWriter, r *http.Request) {
+	db, err := database.ConfigDatabase()
+	if err != nil {
+		fmt.Println("The database connection error:", err)
+		return
+	}
+	defer db.Close()
+	rows, err := db.Query("SELECT * FROM books;")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id string
+		var title string
+		var discard any
+		err := rows.Scan(&id, &title, &discard, &discard)
+		if err != nil {
+			fmt.Println(err)
+		}
+		w.Write([]byte(fmt.Sprintf("id: %s---title:%s \n", id, title)))
+	}
+}
+func testStreamMp3(w http.ResponseWriter, r *http.Request) {
+	audioFile, err := os.Open("/home/iqthuc/Downloads/Nang-Tho-Hoang-Dung.mp3")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer audioFile.Close()
+	w.Header().Set("Content-Type", "audio/mpeg")
+
+	_, err = io.Copy(w, audioFile)
+	if err != nil {
+		http.Error(w, "Unable to stream MP3", http.StatusInternalServerError)
+	}
+}
+func testApiMp3(w http.ResponseWriter, r *http.Request) {
+	audioFile, err := os.ReadFile("/home/iqthuc/Downloads/Nang-Tho-Hoang-Dung.mp3")
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "audio/mpeg")
+	w.Write(audioFile)
+}
+
+func main() {
+	mainRouter := mux.NewRouter()
+
+	homeRouter := mainRouter.PathPrefix("/home").Subrouter()
+
+	homeRouter.HandleFunc("", getSomeData)
+	homeRouter.HandleFunc("/test-mp3", testApiMp3)
+	homeRouter.HandleFunc("/test-stream-api", testStreamMp3)
+
+	mainServer := http.Server{
+		Addr:    ":3060",
+		Handler: mainRouter,
+	}
+	mainServer.ListenAndServe()
+}
