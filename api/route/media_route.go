f2b2f37d0ba37da50fe3aa6b6c9deb6ac7d68e16
iqthuc <quangthuc2506@gmail.com>
2024-02-04 23:50:48 +0700

    thay http bằng mux

--- api/route/media_route.go
+++ api/route/media_route.go
@@ -1,21 +1,14 @@
 package route
 
 import (
-	controller "eng_app_module/api/controller/media"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func NewMediaRoute(server *http.ServeMux) {
-	audioRoutePrefix := "/media/"
+func NewMediaRoute(group *mux.Router) {
+	mediaDirectory := "/home/iqthuc/Downloads/data compiled/resources/assets/AmericanStreamline/"
 
-	server.HandleFunc(audioRoutePrefix, controller.RequestMedia)
-
+	mediaRoute := group.PathPrefix("/media").Subrouter()
+	mediaRoute.PathPrefix("/audio/{filepath:.+}").Handler(http.StripPrefix("/media/audio", http.FileServer(http.Dir(mediaDirectory))))
 }
-
-// func NewMediaRoute(group *mux.Router) {
-// 	mediaDirectory := "/home/iqthuc/Downloads/data compiled/resources/assets/AmericanStreamline/"
-
-// 	mediaRoute := group.PathPrefix("/media").Subrouter()
-
-// 	mediaRoute.PathPrefix("/audio/{filepath:.+}").Handler(http.StripPrefix("/media/audio", http.FileServer(http.Dir(mediaDirectory))))
-// }
--- api/route/streamline_book_route.go
+++ api/route/streamline_book_route.go
@@ -7,16 +7,8 @@ import (
 )
 
 func NewStreamlineBookRoute(groupRouter *mux.Router) {
-
-	routePrefix := "/streamline"
-	server.HandleFunc(routePrefix+"/lessons", controller.GetLessons)
-	server.HandleFunc(routePrefix+"/lessons/", controller.GetLessonDetail)
-	server.HandleFunc(routePrefix+"/books", controller.GetBooks)
+	streamlineBookRoute := groupRouter.PathPrefix("/streamline").Subrouter()
+	streamlineBookRoute.Path("/lessons").HandlerFunc(controller.GetLessons).Methods("GET")
+	streamlineBookRoute.Path("/lessons/{id}").HandlerFunc(controller.GetLessonDetail).Methods("GET")
+	streamlineBookRoute.Path("/books").HandlerFunc(controller.GetBooks).Methods("GET")
 }
-
-// func NewStreamlineBookRoute(group *mux.Router) {
-// 	streamlineBookRoute := group.PathPrefix("/streamline").Subrouter()
-// 	streamlineBookRoute.Path("/lessons").HandlerFunc(controller.GetLessons).Methods("GET")
-// 	streamlineBookRoute.Path("/lessons/{id}").HandlerFunc(controller.GetLessonDetail).Methods("GET")
-// 	streamlineBookRoute.Path("/books").HandlerFunc(controller.GetBooks).Methods("GET")
-// }
